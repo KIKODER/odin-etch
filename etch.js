2457c61122c6fd4621713dfbff6136c7b2afac9f
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-04-20 20:06:47 -0500

    Added prompting functionallity and changed properties of the squares.

--- etch.js
+++ etch.js
@@ -1,12 +1,17 @@
 const container = document.querySelector("#container");
 
-//let numEntered = prompt("Enter a number between 1-100.");
-//let size = numEntered * numEntered;
+let numEntered = prompt("Enter a number between 1-100.");
+let sizeGrid = numEntered * numEntered;
 
-for (let i = 0; i < 256; i++){
+let pageSize = 100;
+let sizeSquare = pageSize / numEntered;
+
+for (let i = 0; i < sizeGrid; i++){
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = `${sizeSquare}vw`;
+    square.style.height = `${sizeSquare}vh`;
     container.appendChild(square);
 }
 
-//console.log(size);
+console.log(sizeGrid);
