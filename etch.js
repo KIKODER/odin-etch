f72fe6b80c8f8fcc08969b2d65c5704fae09eab0
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-04-30 15:51:11 -0500

    Updated/fixed .js and .css files to properly show the grid of divs. Page now creates proper grid based on user input.

--- etch.js
+++ etch.js
@@ -1,17 +1,15 @@
 const container = document.querySelector("#container");
 
-let numEntered = prompt("Enter a number between 1-100.");
-let sizeGrid = numEntered * numEntered;
+let numEntered = prompt("Enter a number between 1 and 100.");
+numEntered = Math.min(Math.max(parseInt(numEntered), 1), 100); // clamp between 1 and 100
 
-let pageSize = 100;
-let sizeSquare = pageSize / numEntered;
+const totalSize = 90; // using 90vw and 90vh as max container size
+const squareSize = totalSize / numEntered;
 
-for (let i = 0; i < sizeGrid; i++){
+for (let i = 0; i < numEntered * numEntered; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.style.width = `${sizeSquare}vw`;
-    square.style.height = `${sizeSquare}vh`;
+    square.style.width = `${squareSize}vw`;
+    square.style.height = `${squareSize}vh`;
     container.appendChild(square);
 }
-
-console.log(sizeGrid);
