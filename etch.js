0e80c4d71a5622fddbfadf0552a51d8eb6c4eff7
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-05-01 20:26:19 -0500

    Added a reset button that clears the page upon function.

--- etch.js
+++ etch.js
@@ -1,9 +1,10 @@
 const container = document.querySelector("#container");
 
 let numEntered = prompt("Enter a number between 1 and 100.");
-numEntered = Math.min(Math.max(parseInt(numEntered), 1), 100); // clamp between 1 and 100
-
-const totalSize = 90; // using 90vw and 90vh as max container size
+// clamp between 1 and 100
+numEntered = Math.min(Math.max(parseInt(numEntered), 1), 100); 
+// using 90vw and 90vh as max container size
+const totalSize = 90; 
 const squareSize = totalSize / numEntered;
 
 for (let i = 0; i < numEntered * numEntered; i++) {
@@ -13,7 +14,13 @@ for (let i = 0; i < numEntered * numEntered; i++) {
     square.style.height = `${squareSize}vh`;
     container.appendChild(square);
     square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = 'blue';
+        square.style.backgroundColor = 'gray';
     });
 }
 
+const reset = document.querySelector("#reset");
+
+reset.addEventListener("click", () => {
+    console.log("Clicked!");
+    document.querySelector("#container").innerHTML = "";
+});
