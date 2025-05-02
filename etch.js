fcb3cab321ab6ae194de133e70426713b39731f5
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-05-01 23:41:41 -0500

    Organized .js file and completed the reset button's functionality.

--- etch.js
+++ etch.js
@@ -1,26 +1,31 @@
 const container = document.querySelector("#container");
-
-let numEntered = prompt("Enter a number between 1 and 100.");
-// clamp between 1 and 100
-numEntered = Math.min(Math.max(parseInt(numEntered), 1), 100); 
-// using 90vw and 90vh as max container size
-const totalSize = 90; 
-const squareSize = totalSize / numEntered;
-
-for (let i = 0; i < numEntered * numEntered; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = `${squareSize}vw`;
-    square.style.height = `${squareSize}vh`;
-    container.appendChild(square);
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = 'gray';
-    });
-}
-
 const reset = document.querySelector("#reset");
 
+function generateGrid(numEntered) {
+    // using 90vw and 90vh as max container size
+    const totalSize = 90; 
+    const squareSize = totalSize / numEntered;
+
+    for (let i = 0; i < numEntered * numEntered; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}vw`;
+        square.style.height = `${squareSize}vh`;
+        container.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = 'gray';
+        });
+    }
+}
+
+let numEntered = prompt("Enter a number between 1 and 100.");
+numEntered = Math.min(Math.max(parseInt(numEntered), 1), 100);  // clamp between 1 and 100
+generateGrid(numEntered);
+
 reset.addEventListener("click", () => {
-    console.log("Clicked!");
     document.querySelector("#container").innerHTML = "";
+    let numEntered = prompt("Enter a number between 1 and 100.");
+    const totalSize = 90; 
+    const squareSize = totalSize / numEntered;
+    generateGrid(numEntered);
 });
