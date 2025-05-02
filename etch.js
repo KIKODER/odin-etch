ee866d0a560435080e6691f10e81d69cd5dc23b3
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-05-01 23:58:52 -0500

    Fixed the grid layout so the divs and the container are perfectly square.

--- etch.js
+++ etch.js
@@ -2,15 +2,13 @@ const container = document.querySelector("#container");
 const reset = document.querySelector("#reset");
 
 function generateGrid(numEntered) {
-    // using 90vw and 90vh as max container size
-    const totalSize = 90; 
-    const squareSize = totalSize / numEntered;
+    const squareSize = 500 / numEntered;
 
     for (let i = 0; i < numEntered * numEntered; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        square.style.width = `${squareSize}vw`;
-        square.style.height = `${squareSize}vh`;
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         container.appendChild(square);
         square.addEventListener("mouseover", () => {
             square.style.backgroundColor = 'gray';
@@ -25,7 +23,5 @@ generateGrid(numEntered);
 reset.addEventListener("click", () => {
     document.querySelector("#container").innerHTML = "";
     let numEntered = prompt("Enter a number between 1 and 100.");
-    const totalSize = 90; 
-    const squareSize = totalSize / numEntered;
     generateGrid(numEntered);
 });
